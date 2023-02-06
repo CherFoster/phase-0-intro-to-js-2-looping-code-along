806a9fe5622e1f25879812550a5cad97fd69f9a9
Cherise Foster <cherise.foster@outlook.com>
2023-02-06 15:18:23 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    console.log(cards);
+    }
+    return cards;
+}
+
+writeCards(names, "surprise");
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count);
+        count = count -1;
+    }
+}
+countDown();
